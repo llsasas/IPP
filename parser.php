4460e20e2ae17c91d1f66cd9d481b8439b1306ff
llsasas <94858035+llsasas@users.noreply.github.com>
2023-02-18 21:44:17 +0100

    Create parser.php
    Initial commit, created basic structure of the project

---
+++ parser.php
@@ -0,0 +1,97 @@
+<?php
+
+/*if($argc > 1)
+{
+    if($argv[2] == "--help")
+        echo("Usage:")
+        #exit(1);
+    
+}*/
+
+/*$header = false;
+if(fgets(STDIN) == ".IPPcode23")
+{
+$header = true;
+}
+else
+{
+    exit(21);
+}*/
+
+$line
+
+while ($line = fgets(STDIN))
+{
+    $token = explode('', trim($line, '\n'))
+    switch(strtoupper($tokens[0]))
+    {
+        case 'MOVE':
+            break;
+        case 'CREATEFRAME':
+            break;
+        case 'PUSHFRAME':
+            break;
+        case 'POPFRAME':
+            break;
+        case 'DEFVAR':
+            break;
+        case 'CALL':
+            break;
+        case 'RETURN':
+            break;
+        case 'PUSHS':
+            break;
+        case 'POPS':
+            break;
+        case 'ADD':
+            break;
+        case 'SUB':
+            break;
+        case 'MUL':
+            break;
+        case 'IDIV':
+            break;
+        case 'LT':
+            break;
+        case 'GT':
+            break;
+        case 'EQ':
+            break;
+        case 'AND':
+            break;
+        case 'OR':
+            break;
+        case 'NOT':
+            break;
+        case 'INT2CHAR':
+            break;
+        case 'STRI2INT':
+            break;
+        case 'READ':
+            break;
+        case 'WRITE':
+            break;
+        case 'CONCAT':
+            break;
+        case 'STRLEN':
+            break;
+        case 'GETCHAR':
+            break;
+        case 'SETCHAR':
+            break;
+        case 'TYPE':
+            break;
+        case 'LABEL':
+            break;
+        case 'JUMP':
+            break;
+        case 'JUMPIFEQ':
+            break;
+        case 'JUMPIFNEQ':
+            break;
+        case 'EXIT':
+            break;
+    }
+}
+
+?>
