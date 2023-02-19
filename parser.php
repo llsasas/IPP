71946c39a6978c45e7affca45a64a58aca1954b2
llsasas <94858035+llsasas@users.noreply.github.com>
2023-02-19 13:22:33 +0100

    Created additional functions

--- parser.php
+++ parser.php
@@ -1,44 +1,92 @@
 <?php
 
 ini_set('display_errors', 'stderr');
-echo("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
+echo("<?xml version=\"1.0\" encoding=\"UTF-8\"?> \n");
+echo("<program language = \"IPPcode23\">");
 if($argc > 1)
 {
     if($argv[2] == "--help")
         echo("Usage: parser.php "); //TODO
-        exit(1);
+        exit(0);
     
 }
 
 $header = false;
-if(fgets(STDIN) == ".IPPcode23")
-{
-$header = true;
-}
-else
+if(fgets(STDIN) != ".IPPcode23")
 {
     exit(21);
 }
+else
+{
 
+}
+
+function is_variable($string)
+{
+    if(preg_match("/(LF || TF || GF)@([_\-$#%*!?A-Za-z]([A-Za-z0-9_\-$#%*!?]))* */",$string[1]))
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function is_symbol($string)
+{
+    if(true)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+function instruction($num,$arguments,$name)
+{
+    echo("<instruction order=\"$num\" opcode\"$name\">\n");
+    //TODO arguments
+    echo("<//instruction>");
+}
 $line;
-
+$instrctnum = 0;
 while ($line = fgets(STDIN))
 {
+    $instrctnum++;
     $token = explode('', trim($line, '\n'));
     switch(strtoupper($tokens[0]))
     {
         case 'MOVE':
+            if(is_variable($line[1]))
+            {
+                //todo
+            }
+            else
+            {
+                exit(23);
+            }
             break;
-        case 'CREATEFRAME':
+      /*  case 'CREATEFRAME':
             break;
         case 'PUSHFRAME':
             break;
         case 'POPFRAME':
-            break;
+            break;*/
         case 'DEFVAR':
+            if(is_variable($line[1]))
+            {
+                //TODO
+            }
+            else
+            {
+                exit(23);
+            }
             break;
-        case 'CALL':
-            break;
+     /*   case 'CALL':
+            break;*/
         case 'RETURN':
             break;
         case 'PUSHS':
@@ -92,6 +140,8 @@ while ($line = fgets(STDIN))
         case 'JUMPIFNEQ':
             break;
         case 'EXIT':
+            break;
+        case 'DPRINT':
             break;
     }
 }
