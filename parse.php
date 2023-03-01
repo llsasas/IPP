f4b0054ce76cd89da9dddf29528eb877269bd3b9
llsasas <94858035+llsasas@users.noreply.github.com>
2023-03-01 17:56:55 +0100

    Fixed errors

--- parse.php
+++ parse.php
@@ -7,6 +7,7 @@ $xml_output = "";
 // Attaches given string to an output xml file that will be printed at the end of the program
 function attach_to_output($current, $to_attach)
 {
+    fwrite(STDERR, "10");
     $current = $current . $to_attach . "\n";
     return $current;
 }
@@ -19,12 +20,11 @@ if ($argc > 1) {
 }
 
 $xml_output = attach_to_output($xml_output, "<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
-$xml_output = attach_to_output($xml_output, "<program language = \"IPPcode23\">");
 
 // Erases the comments from the given line
-function erase_comments($line)
+function erase_comments($string)
 {
-    preg_replace("/#.*/", '', $line);
+    return preg_replace("/#.*/", '', $string);    
 }
 
 // Syntax check of variables
@@ -37,9 +37,19 @@ function is_variable($name)
     }
 }
 
+function remove_whitespace($line)
+{
+    $pattern = '/[ \t]+/';
+    $line = preg_replace($pattern, ' ', $line);
+    $pattern = '/[ \t]+$/';
+    $line = preg_replace($pattern, '', $line);
+    return $line;
+}
+
 // Syntax check of labels
 function is_label($name)
 {
+    fwrite(STDERR, "54");
     if (preg_match("/([_\-$#%*!?A-Za-z]([A-Za-z0-9_\-$#%*!?]))* /", $name)) {
         return 0;
     } else {
@@ -50,6 +60,7 @@ function is_label($name)
 // Syntax check of symbols
 function is_symbol($name)
 {
+    fwrite(STDERR, "65");
     if (preg_match("/(LF | TF | GF)@([_\-$#%*!?A-Za-z]([A-Za-z0-9_\-$#%*!?])*) /", $name)) {
         return 0;
     }
@@ -72,6 +83,7 @@ function is_symbol($name)
 
 function is_type($name)
 {
+    fwrite(STDERR, "88");
     switch ($name) {
         case 'int':
         case 'bool':
@@ -84,44 +96,62 @@ function is_type($name)
 
 function instruction($num, $tokens, $numofarguments, $argstype)
 {
+    fwrite(STDERR, "101");
     global $xml_output;
-    $xml_output = attach_to_output($xml_output, "<instruction order=\"$num\" opcode=\" $tokens[0] \">\n");
+    $xml_output = attach_to_output($xml_output, "<instruction order=\"$num\" opcode=\"$tokens[0]\">");
     switch ($numofarguments) {
         case '1':
-            $xml_output = attach_to_output($xml_output, "<arg1 type=\"$argstype[0]\"> $tokens[1]</arg1>\n");
+            $xml_output = attach_to_output($xml_output, "<arg1 type=\"$argstype[0]\">$tokens[1]</arg1>");
             break;
         case '2':
-            $xml_output = attach_to_output($xml_output, "<arg2 type=\"$argstype[1]\"> $tokens[2]</arg2>\n");
+            $xml_output = attach_to_output($xml_output, "<arg1 type=\"$argstype[0]\">$tokens[1]</arg1>");
+            $xml_output = attach_to_output($xml_output, "<arg2 type=\"$argstype[1]\">$tokens[2]</arg2>");
             break;
         case '3':
-            $xml_output = attach_to_output($xml_output, "<arg3 type=\"$argstype[2]\"> $tokens[3]</arg3>\n");
+            $xml_output = attach_to_output($xml_output, "<arg1 type=\"$argstype[0]\">$tokens[1]</arg1>");
+            $xml_output = attach_to_output($xml_output, "<arg2 type=\"$argstype[1]\">$tokens[2]</arg2>");
+            $xml_output = attach_to_output($xml_output, "<arg3 type=\"$argstype[2]\">$tokens[3]</arg3>");
             break;
     }
-    echo ("<//instruction>");
+    $xml_output = attach_to_output($xml_output, "</instruction>");
 }
 
 $header = false;
-$line;
 $instrctnum = 0;
-$arg_num = 0;
+
 while ($line = fgets(STDIN)) {
-    if (!$header) {
-        $line = strtoupper($line);
-        if ($line == ".IPPCODE23") {
+   $line = erase_comments($line);
+   $line = remove_whitespace($line);
+   $line = preg_replace("/&/",'&amp;',$line);                       #nacitanie po riadku zo vstupu, reg. vyrazy na upravu riadku (odkomentovanie, zamenenie zanakov)
+   $line = preg_replace("/</",'&lt;',$line);
+   $line = preg_replace("/>/",'&gt;',$line);
+   $line = preg_replace('/#.*/','',preg_replace('#//.*#','',preg_replace('#/\*(?:[^*]*(?:\*(?!/))*)*\*//#','',($line))));
+   
+    if ($header == false && $line!= "\n") {
+        if ((strtoupper(trim($line, "\n")) == ".IPPCODE23") || (strtoupper(trim($line, "\n")) == ".IPPCODE23")) {
             $header = true;
-        } else {
+            $xml_output = attach_to_output($xml_output, "<program language=\"IPPcode23\">");
+            continue;
+        } 
+        else 
+        {
             exit(21);
         }
     }
+    if($line == "\n")
+    {
+        continue;
+    }
     $instrctnum++;
-    $token = explode('', trim($line, '\n'));
-    count($token);
+    $token = array_values(array_filter(explode(' ',trim($line, "\n"))));
+    $arg_num = count($token) - 1;
     switch (strtoupper($token[0])) {
         // <var> <symb>
         case 'MOVE':
         case 'INT2CHAR':
         case 'STRLEN':
         case 'TYPE':
+            $argstype = array("var", "symb");
             if($arg_num != 2)
             {
                 exit(23);
@@ -142,6 +172,7 @@ while ($line = fgets(STDIN)) {
         // <var>
         case 'DEFVAR':
         case 'POPS':
+            $argstype = array("var");
             if($arg_num != 1)
             {
                 exit(23);
@@ -157,6 +188,7 @@ while ($line = fgets(STDIN)) {
         case 'CALL':
         case 'LABEL':
         case 'JUMP':
+            $argstype = array("label");
             if($arg_num != 1)
             {
                 exit(23);
@@ -183,6 +215,7 @@ while ($line = fgets(STDIN)) {
         case 'CONCAT':
         case 'GETCHAR':
         case 'SETCHAR':
+            $argstype = array("var", "symb1", "symb2");
             if($arg_num != 3)
             {
                 exit(23);
@@ -190,6 +223,7 @@ while ($line = fgets(STDIN)) {
             if (is_variable($token[1])) {
                 if (is_symbol($token[2])) {
                     if (is_symbol(($token[3]))) {
+
                         instruction($instrctnum, $token, $arg_num,$argstype);
                     } else {
                         exit(23);
@@ -204,11 +238,12 @@ while ($line = fgets(STDIN)) {
 
         // <var> <type>
         case 'READ':
+            $argstype = array("var", "type");
             if($arg_num != 2)
             {
                 exit(23);
             }
-            if (is_variable(token[1])) {
+            if (is_variable($token[1])) {
                 instruction($instrctnum, $token, $arg_num,$argstype);
             } else {
                 exit(23);
@@ -218,6 +253,7 @@ while ($line = fgets(STDIN)) {
         // <label> <symb1> <symb2>
         case 'JUMPIFEQ':
         case 'JUMPIFNEQ':
+            $argstype = array("label", "symb1", "symb2");
             if($arg_num != 3)
             {
                 exit(23);
@@ -233,6 +269,7 @@ while ($line = fgets(STDIN)) {
                     exit(23);
                 }
             } else {
+
                 exit(23);
             }
             break;
@@ -242,6 +279,7 @@ while ($line = fgets(STDIN)) {
         case 'WRITE':
         case 'DPRINT':
         case 'PUSHS':
+            $argstype = array("symb");
             if($arg_num != 1)
             {
                 exit(23);
@@ -256,6 +294,9 @@ while ($line = fgets(STDIN)) {
             exit(22); //TODO is this the correct return code for unknown instruction?
     }
 }
+$xml_output = attach_to_output($xml_output, "</program>");
+echo($xml_output);
 
+exit(0);
 
 ?>
